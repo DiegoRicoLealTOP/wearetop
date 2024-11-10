2dc7715edb7171aaba6657f9970c100b2ba4a38f
Cristhian Melo <rxtsel@hotmail.com>
2024-11-09 19:13:23 -0500

    feat(ci): add custom domain and  workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://new.wearetop.co",
+  base: "/",
   integrations: [tailwind(), icon()],
 });
