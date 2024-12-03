117d61d3e03138ad3cda0e6db4433eb73ced52d2
Cristhian Melo <rxtsel@hotmail.com>
2024-12-03 18:54:30 -0500

    refactor(ui): refactor scroll animation

--- src/components/higher-education/show-pricing.svelte
+++ src/components/higher-education/show-pricing.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
   import NumberFlow from '@number-flow/svelte'
   import { onDestroy } from 'svelte';
-  
+
   export let usd: number;
   export let aud: number;
-  
+
   let currentCurrency: "usd" | "aud" = "usd";
   let value = usd;
-  
+
   // Variable para manejar el intervalo
   let intervalId: number;
 
@@ -16,8 +16,8 @@
     currentCurrency = currentCurrency === "usd" ? "aud" : "usd"
     value = currentCurrency === "usd" ? usd : aud
   }
-  
-  const SECONDS = 5 * 1000; // min * sec
+
+  const SECONDS = 8 * 1000; // sec * mils
 
   // Iniciar el intervalo cuando el componente se monta
   intervalId = setInterval(toggleCurrency, SECONDS);
@@ -28,8 +28,8 @@
   });
 </script>
 
-<div 
-  class="space rounded-2xl bg-white/20 px-4 py-2 text-center text-3xl font-black text-white drop-shadow-md backdrop-blur-sm cursor-pointer"
+<div
+  class="space rounded-2xl bg-black/10 px-4 py-2 text-center text-3xl font-black text-white drop-shadow-md backdrop-blur-sm cursor-pointer"
   onclick={toggleCurrency}
 >
   <NumberFlow
