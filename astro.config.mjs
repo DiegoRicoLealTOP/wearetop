7436e8682ac0a58fd3405b45bc88c26fb337b07e
Cristhian Melo <rxtsel@outlook.com>
2026-04-29 17:16:13 -0500

    feat(seo): add mising seo files

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,12 +7,15 @@ import svelte from "@astrojs/svelte";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://wearetop.co",
   base: "/",
-  integrations: [icon(), svelte()],
+  integrations: [icon(), svelte(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
 });
+
